1228ff5e62700dc8bf4b638f671954f36e910a2a
Dylan <leehc12dl@naver.com>
2026-01-08 16:26:45 +0900

    first commit

---
+++ popeye-backend/src/main/java/popeye/popeyebackend/PopeyeBackendApplication.java
@@ -0,0 +1,13 @@
+package popeye.popeyebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopeyeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopeyeBackendApplication.class, args);
+    }
+
+}
---
+++ popeye-backend/src/test/java/popeye/popeyebackend/PopeyeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package popeye.popeyebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopeyeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
