1c561d9a8c3cbd72f5bbf05cbbc138096cdeac37
Dylan <leehc12dl@naver.com>
2026-01-11 00:26:53 +0900

    batch 설정

--- popeye-backend/src/main/java/popeye/popeyebackend/batch/statistics/StatisticsBatchConfig.java
+++ popeye-backend/src/main/java/popeye/popeyebackend/batch/statistics/StatisticsBatchConfig.java
@@ -3,10 +3,10 @@ package popeye.popeyebackend.batch.statistics;
 import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.job.Job;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.step.Step;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +19,6 @@ public class StatisticsBatchConfig {
 
     private final JobRepository jobRepository;
     private final PlatformTransactionManager transactionManager;
-    private final EntityManagerFactory entityManagerFactory;
 
     private final DailyStatsTasklet dailyStatsTasklet;
 
