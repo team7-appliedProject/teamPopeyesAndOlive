6676d2b2a521eb449fc8aba0ead29d2b61b75396
tory <chkoreakr@google.com>
2026-01-12 17:09:12 +0900

    refactor : 유저레포 로그인-이메일, 중복가입체크(닉네임,이메일 추가)

--- popeye-backend/src/main/java/popeye/popeyebackend/user/repository/UserRepository.java
+++ popeye-backend/src/main/java/popeye/popeyebackend/user/repository/UserRepository.java
@@ -6,8 +6,16 @@ import org.springframework.data.repository.query.Param;
 import popeye.popeyebackend.user.domain.User;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    // 로그인을 위한 이메일 조회
+    Optional<User> findByEmail(String email);
+
+    // 중복 가입 체크(닉네임, 이메일)
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
 
     @Query("select count(u) from User u " +
             "where u.createdAt between :start and :end ")
