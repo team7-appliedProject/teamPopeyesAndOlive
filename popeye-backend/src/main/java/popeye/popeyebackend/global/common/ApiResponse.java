09ceecb10171ac540bd8294d02accf6c3f2c3053
tory <chkoreakr@google.com>
2026-01-13 08:45:32 +0900

    feat : ApiResponse(status, message, data)

---
+++ popeye-backend/src/main/java/popeye/popeyebackend/global/common/ApiResponse.java
@@ -0,0 +1,32 @@
+package popeye.popeyebackend.global.common;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiResponse<T> {
+
+    private String status;
+    private String message;
+    private T data;
+
+    private ApiResponse(String status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>("success", null, data);
+    }
+
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>("success", message, data);
+    }
+
+    public static ApiResponse<?> error(String message) {
+        return new ApiResponse<>("error", message, null);
+    }
+}
