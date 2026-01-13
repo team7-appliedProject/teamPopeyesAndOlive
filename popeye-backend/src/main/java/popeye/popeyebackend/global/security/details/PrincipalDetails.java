ff11ea20006ac5fcaa0cacbf56869c62aaabc06c
Dylan <leehc12dl@naver.com>
2026-01-13 10:58:21 +0900

    추후 이어서 구현

---
+++ popeye-backend/src/main/java/popeye/popeyebackend/global/security/details/PrincipalDetails.java
@@ -0,0 +1,35 @@
+package popeye.popeyebackend.global.security.details;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import popeye.popeyebackend.user.domain.User;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class PrincipalDetails implements UserDetails {
+    private final User user;
+    public PrincipalDetails(User user) {
+        this.user = user;
+    }
+
+    public Long getUserId() {
+        return user.getId();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+}
