842174a572aa2e14c33ca0effdb4601074b05a15
tory <chkoreakr@google.com>
2026-01-14 15:20:24 +0900

    refactor : 크리에이터 권한 신청 및 승격에 대한 패치 추가

--- popeye-backend/src/main/java/popeye/popeyebackend/user/controller/UserController.java
+++ popeye-backend/src/main/java/popeye/popeyebackend/user/controller/UserController.java
@@ -1,13 +1,26 @@
 package popeye.popeyebackend.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import popeye.popeyebackend.global.common.ApiResponse;
 import popeye.popeyebackend.user.service.UserService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/users")
 public class UserController {
+
     private final UserService userService;
 
-
+    //크리에이터 권한 신청 및 승격
+    @PatchMapping("/me/creator")
+    public ApiResponse<Void> promoteToCreator(@AuthenticationPrincipal UserDetails userDetails) {
+        userService.promoteToCreator(userDetails.getUsername());
+        //void?
+        return ApiResponse.<Void>success("크리에이터 권한으로 승격되었습니다.", null);
+    }
 }
