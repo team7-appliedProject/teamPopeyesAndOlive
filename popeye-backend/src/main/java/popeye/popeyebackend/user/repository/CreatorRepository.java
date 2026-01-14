cf476f898b15d00879e8ee3e4c6ae14d9ac0e3fd
tory <chkoreakr@google.com>
2026-01-14 15:17:41 +0900

    refactor : 크리에이터 정보 확인

--- popeye-backend/src/main/java/popeye/popeyebackend/user/repository/CreatorRepository.java
+++ popeye-backend/src/main/java/popeye/popeyebackend/user/repository/CreatorRepository.java
@@ -2,6 +2,9 @@ package popeye.popeyebackend.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import popeye.popeyebackend.user.domain.Creator;
+import popeye.popeyebackend.user.domain.User;
 
 public interface CreatorRepository extends JpaRepository<Creator,Long> {
+    // 승격 유저에게 크리에이터 정보 있는지 판단
+    boolean existsByUser(User user);
 }
