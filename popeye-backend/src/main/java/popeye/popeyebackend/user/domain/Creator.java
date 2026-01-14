691b7b032e49a53b2282c0742a7ac073ff75c3e0
tory <chkoreakr@google.com>
2026-01-14 15:17:22 +0900

    refactor : 애노테이션 추가, 은행명, 계좌 수정, 팩토리메서드 (유저->크리에이터)

--- popeye-backend/src/main/java/popeye/popeyebackend/user/domain/Creator.java
+++ popeye-backend/src/main/java/popeye/popeyebackend/user/domain/Creator.java
@@ -1,28 +1,40 @@
 package popeye.popeyebackend.user.domain;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import popeye.popeyebackend.content.domain.Content;
-
 import java.util.List;
 
 @Entity
 @Getter
 @Table(name = "creators")
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //외부 객체생성 방지
+@AllArgsConstructor
+@Builder
 public class Creator {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @Column(length = 500)
+    //정산계좌정보
+    @Column(length = 500, nullable = true)
     private String account;
+
+    //은행이름
+    @Column(length = 100)
+    private String bank_name;
 
     @OneToMany(mappedBy = "creator")
     private List<Content> contents;
+
+    //정적팩토리 메서드 유저객체->크리에이터 객체
+    public static Creator from(User user) {
+        return Creator.builder()
+                .user(user)
+                .build();
+    }
 }
