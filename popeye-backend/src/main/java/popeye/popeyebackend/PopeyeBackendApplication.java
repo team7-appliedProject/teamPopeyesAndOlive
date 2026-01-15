7ba26b77300817fc057f37c5dbdb664e3d8dde13
Dylan <leehc12dl@naver.com>
2026-01-15 15:24:59 +0900

    import수정

--- popeye-backend/src/main/java/popeye/popeyebackend/PopeyeBackendApplication.java
+++ popeye-backend/src/main/java/popeye/popeyebackend/PopeyeBackendApplication.java
@@ -1,15 +1,8 @@
 package popeye.popeyebackend;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
