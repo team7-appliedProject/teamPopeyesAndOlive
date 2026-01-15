655c15d04cbc74811bcff8702de54676e4c4eafd
Dylan <leehc12dl@naver.com>
2026-01-15 14:22:58 +0900

    매개변수 추가

--- popeye-backend/src/main/java/popeye/popeyebackend/pay/controller/EventController.java
+++ popeye-backend/src/main/java/popeye/popeyebackend/pay/controller/EventController.java
@@ -2,10 +2,12 @@ package popeye.popeyebackend.pay.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import popeye.popeyebackend.global.security.details.PrincipalDetails;
 import popeye.popeyebackend.pay.service.EventRewardService;
 import popeye.popeyebackend.user.domain.User;
 import popeye.popeyebackend.user.repository.UserRepository;
@@ -18,7 +20,9 @@ public class EventController {
     private final UserRepository userRepository;
 
     @PostMapping("/free-credits")
-    public ResponseEntity<Long> grantFreeCredits(@RequestParam int amount){
+    public ResponseEntity<Long> grantFreeCredits(
+            @AuthenticationPrincipal PrincipalDetails principalDetails,
+            @RequestParam int amount){
         // 임시 사용자
         User user = userRepository.findById(1L).orElseThrow();
 
