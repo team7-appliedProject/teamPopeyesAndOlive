4aefe776eef5632286c1e868cb8e432bd000fdcd
tory <chkoreakr@google.com>
2026-01-17 18:25:36 +0900

    refactor : 로그인 세터 추가

--- popeye-backend/src/main/java/popeye/popeyebackend/user/dto/request/LoginRequest.java
+++ popeye-backend/src/main/java/popeye/popeyebackend/user/dto/request/LoginRequest.java
@@ -3,8 +3,10 @@ package popeye.popeyebackend.user.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class LoginRequest {
 
